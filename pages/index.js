de9b9a8da0b7852a5169e27aeab05210e7186bba
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-03-07 16:32:28 -0500

    pair-programming-article

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { HOME_OG_IMAGE_URL } from '../lib/constants'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import { HOME_OG_IMAGE_URL } from "../lib/constants";
+import { getAllPosts } from "../lib/api";
+import Head from "next/head";
 
 export default function Index({ allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout>
         <Head>
           <title>Developing Matt</title>
           <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+          <meta
+            name="description"
+            content={`A blog detailing Matt's journey as an aspiring software developer.`}
+          />
         </Head>
         <Container>
           <Intro />
@@ -33,20 +37,20 @@ export default function Index({ allPosts }) {
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps() {
   const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
+    "title",
+    "date",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
+  ]);
 
   return {
     props: { allPosts },
-  }
+  };
 }
